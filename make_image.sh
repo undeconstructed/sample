9da37f4d8078b00a314354b884a2aeb307131cdd
Phil Housley <undeconstructed@gmail.com>
2019-09-23 02:46:34 +0100

    Entrypoint, that's the word

--- make_image.sh
+++ make_image.sh
@@ -2,9 +2,12 @@
 imagename=$1
 
 container=$(buildah from scratch)
-buildah config --cmd /usr/bin/app $container
-buildah config --created-by "creator" $container
-buildah config --author "author at place" --label name=$imagename $container
+buildah config \
+  --entrypoint '[ "/usr/bin/app" ]' \
+  --created-by "a script" \
+  --author "Phil <address>" \
+  --label name=$imagename \
+  $container
 buildah copy $container ./_build/app /usr/bin/
 buildah unmount $container
 image=$(buildah commit $container $imagename)
